64dc1a8d3d783f19a4709f570e200841eccb83be
Jose <eulzmagnaye@gmail.com>
2023-09-27 20:14:05 +0800

    final build

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+
+export {
+    Hero,
+    Subscribe,
+    Services,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    SpecialOffers,
+    SuperQuality
+}
